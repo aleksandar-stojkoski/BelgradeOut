a74946705896581c82b785563900ed82321ece4b
StrahinjaMilovanovic <ms140463d@student.etf.bg.ac.rs>
2017-06-11 02:48:57 +0200

    Povezivanje sa kontrolorom i bazom
    Implementirano izlistavanje dogadjaja i komentara objekta, kao i prikazivanje informacija

--- Faza 6 - Implementacija/belgradeout/application/views/objekat_registrovani_korisnik.php
+++ Faza 6 - Implementacija/belgradeout/application/views/objekat_registrovani_korisnik.php
@@ -20,7 +20,7 @@
             <!--a class="navbar-brand" href="#">Belgrade<span>Out</span></a> -->
             
             <!-- IMG BASED LOGO  -->
-            <a class="navbar-brand" href="<?php echo base_url();?>IndexController"><img src="img/logo.png" alt="logo"></a> 
+            <a class="navbar-brand" href="<?php echo base_url();?>IndexController"><img src="../../img/logo.png" alt="logo"></a> 
 			
                    
           </div>
@@ -44,17 +44,17 @@
             <div class="about_area">
               <!-- START ABOUT HEADING -->
               <div class="heading">
-					<h2 class="wow fadeInLeftBig">Naziv objekta</h2>
+					<h2 class="wow fadeInLeftBig"> <?php echo $Naziv ?></h2>
 					<div class="team_img">
-						  <img src="img/team-1.jpg" alt="img">
+						  <img src="../../img/<?php echo $Slika ?>" alt="img">
 					</div>
-					<p>	1. Naziv objekta: KST <br>
-						2. Adresa objekta: Bulevar 4 <br>
-						3. Radno vreme objekta: 00:00 - 24:00 <br>
-						4. Kapacitet objekta: 250 <br>
-						5. Tip objekta: klub <br>
-						6. Prosečna ocena: 4 <br><br>
-						Kratak opis objekta: ....
+                                        <p>	<strong> Naziv objekta: </strong> <?php echo $Naziv ?> <br>
+						<strong> Adresa objekta: </strong> <?php echo $Adresa ?> <br>
+						<strong> Radno vreme objekta: </strong> <?php echo $RadnoVreme ?> <br>
+						<strong> Kapacitet objekta: </strong> <?php echo $Kapacitet ?> <br>
+						<strong> Tip objekta: </strong> <?php echo $TipObjekta ?> <br>
+						<strong> Prosečna ocena: </strong> <?php echo $Ocena ?> (od <?php echo $BrGlasova ?> glasova)<br><br>
+						<strong> Kratak opis objekta: </strong> <?php echo $Opis ?>
 					</p>
 					<p>	
 						<br>
@@ -79,7 +79,6 @@
             <!-- START BLOG HEADING -->
             <div class="heading">
               <h2 class="wow fadeInLeftBig">Događaji u objektu</h2>
-              <p>Ovde se nalazi spisak svih događaja u ovom objektu:</p>
             </div>
           </div>
           <div class="col-lg-12 col-md-12">
@@ -88,110 +87,26 @@
 
               <!-- BEGIN BLOG SLIDER -->
               <div class="blog_slider">
-                <!-- BEGIN SINGLE BLOG -->
-                <div class="col-lg-4 col-md-4 col-sm-4">
-                  <div class="single_post wow fadeInUp">
-                    <div class="blog_img">
-                      <img src="img/blog_img1.jpg" alt="img">
-                    </div>
-                    <h3>Događaj 1</h3>
-                    <div class="post_commentbox">
-                      <a href="#"><i class="fa fa-user"></i>Autor</a>
-                      <span><i class="fa fa-calendar"></i>00:00 AM vreme</span>
-                      <a href="#"><i class="fa fa-tags"></i>Tip događaja</a>
-                    </div>
-                    <p>Ovde kratak opis i ispod link ka tom događaju</p>
-                    <a href="#" class="read_more">Otvori događaj <i class="fa fa-angle-double-right"></i></a>
-					
-					                   
-                  </div>
-                </div>
-
-                <!-- BEGIN SINGLE BLOG -->
-                <div class="col-lg-4 col-md-4 col-sm-4">
-                  <div class="single_post wow fadeInUp">
-                    <div class="blog_img">
-                      <img src="img/blog_img1.jpg" alt="img">
-                    </div>
-                    <h3>Događaj 2</h3>
-                    <div class="post_commentbox">
-                      <a href="#"><i class="fa fa-user"></i>Autor</a>
-                      <span><i class="fa fa-calendar"></i>00:00 AM vreme</span>
-                      <a href="#"><i class="fa fa-tags"></i>Tip događaja</a>
-                    </div>
-                    <p>Ovde kratak opis i ispod link ka tom događaju</p>
-                    <a href="#" class="read_more">Otvori događaj <i class="fa fa-angle-double-right"></i></a>
-					
-                  </div>
-                </div>
-                <!-- BEGIN SINGLE BLOG -->
-                <div class="col-lg-4 col-md-4 col-sm-4">
-                  <div class="single_post wow fadeInUp">
-                    <div class="blog_img">
-                      <img src="img/blog_img1.jpg" alt="img">
-                    </div>
-                    <h3>Događaj 3</h3>
-                    <div class="post_commentbox">
-                      <a href="#"><i class="fa fa-user"></i>Autor</a>
-                      <span><i class="fa fa-calendar"></i>00:00 AM vreme</span>
-                      <a href="#"><i class="fa fa-tags"></i>Tip događaja</a>
-                    </div>
-                    <p>Ovde kratak opis i ispod link ka tom događaju</p>
-                    <a href="#" class="read_more">Otvori događaj <i class="fa fa-angle-double-right"></i></a>
-					
-                  </div>
-                </div>
-                <!-- BEGIN SINGLE BLOG -->
-                <div class="col-lg-4 col-md-4 col-sm-4">
-                  <div class="single_post wow fadeInUp">
-                    <div class="blog_img">
-                      <img src="img/blog_img1.jpg" alt="img">
-                    </div>
-                    <h3>Događaj 4</h3>
-                    <div class="post_commentbox">
-                      <a href="#"><i class="fa fa-user"></i>Autor</a>
-                      <span><i class="fa fa-calendar"></i>00:00 AM vreme</span>
-                      <a href="#"><i class="fa fa-tags"></i>Tip događaja</a>
-                    </div>
-                    <p>Ovde kratak opis i ispod link ka tom događaju</p>
-                    <a href="#" class="read_more">Otvori događaj <i class="fa fa-angle-double-right"></i></a>
-					
-                  </div>
-                </div>
-                <!-- BEGIN SINGLE BLOG -->
-                <div class="col-lg-4 col-md-4 col-sm-4">
-                  <div class="single_post wow fadeInUp">
-                    <div class="blog_img">
-                      <img src="img/blog_img1.jpg" alt="img">
-                    </div>
-                    <h3>Događaj 5</h3>
-                    <div class="post_commentbox">
-                      <a href="#"><i class="fa fa-user"></i>Autor</a>
-                      <span><i class="fa fa-calendar"></i>00:00 AM vreme</span>
-                      <a href="#"><i class="fa fa-tags"></i>Tip događaja</a>
-                    </div>
-                    <p>Ovde kratak opis i ispod link ka tom događaju</p>
-                    <a href="#" class="read_more">Otvori događaj <i class="fa fa-angle-double-right"></i></a>
-					
-                  </div>
-                </div>
-                <!-- BEGIN SINGLE BLOG -->
-                <div class="col-lg-4 col-md-4 col-sm-4">
-                  <div class="single_post wow fadeInUp">
-                    <div class="blog_img">
-                      <img src="img/blog_img1.jpg" alt="img">
-                    </div>
-                    <h3>Događaj 6</h3>
-                    <div class="post_commentbox">
-                      <a href="#"><i class="fa fa-user"></i>Autor</a>
-                      <span><i class="fa fa-calendar"></i>00:00 AM vreme</span>
-                      <a href="#"><i class="fa fa-tags"></i>Tip događaja</a>
-                    </div>
-                    <p>Ovde kratak opis i ispod link ka tom događaju</p>
-                    <a href="#" class="read_more">Otvori događaj <i class="fa fa-angle-double-right"></i></a>
-					
-                  </div>
-                </div>                
+<!--                 BEGIN SINGLE BLOG -->
+                <?php
+                       $br = count($dogadjaji);
+                       for ($i = 0; $i < $br; $i++){ 
+                                ?>
+                            <div class="col-lg-4 col-md-4 col-sm-4">
+                                <div class="single_post wow fadeInUp">
+                                    <div class="blog_img">
+                                        <img src="../../img/<?php echo $dogadjaji[$i]->Slika ?>" alt="img">
+                                    </div>
+                                    <h3> <?php echo $dogadjaji[$i]->Naziv ?> </h3>
+                                    <div class="post_commentbox">
+                                        <span><i class="fa fa-calendar"></i> <?php echo $dogadjaji[$i]->trajanje ?></span>
+                                        <a href=""><i class="fa fa-tags"></i><?php echo $dogadjaji[$i]->TipDogadjaja ?></a>
+                                    </div>
+                                    <p><?php echo $dogadjaji[$i]->Opis ?></p>
+                                    <a href="<?php echo base_url() ?>/DogadjajGostRegistrovaniKorisnikController/Index/<?php echo $dogadjaji[$i]->IdDogadjaj ?>" class="read_more">Otvori događaj <i class="fa fa-angle-double-right"></i></a>			                   
+                                </div>
+                            </div>
+                <?php } ?>  
               </div>
             </div>
 			
@@ -200,79 +115,6 @@
       </div>
     </section>
     <!--=========== END BLOG SECTION ================-->
-
-    <!--=========== BEGIN CLIENTS SECTION ================-->
-    <section id="clients">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 col-md-12 col-sm-12">
-            <!-- START BLOG HEADING -->
-            <div class="heading">
-              <h2 class="wow fadeInLeftBig">Slike objekta</h2>
-              <p>Ovde stoje neke slike objekta.</p>
-            </div>
-          </div>
-          <div class="col-lg-12 col-md-12 col-sm-12">
-            <div class="clients_content">
-              <div class="row">
-                <!-- BEGIN CLIENTS SLIDER -->
-                <div class="clients_slider">
-                  <!-- BEGIN SINGLE CLIENT SLIDE#1 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                      <img src="img/clients_img1.png" alt="clients Brand">
-                    </div>
-                  </div>
-                  <!-- BEGIN SINGLE CLIENT SLIDE#2 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                      <img src="img/clients_img1.png" alt="clients Brand">
-                    </div>
-                  </div>
-                  <!-- BEGIN SINGLE CLIENT SLIDE#3 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                      <img src="img/clients_img1.png" alt="clients Brand">
-                    </div>
-                  </div>
-                  <!-- BEGIN SINGLE CLIENT SLIDE#4 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                      <img src="img/clients_img1.png" alt="clients Brand">
-                    </div>
-                  </div>
-                   <!-- BEGIN SINGLE CLIENT SLIDE#5 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                      <img src="img/clients_img1.png" alt="clients Brand">
-                    </div>
-                  </div>
-                  <!-- BEGIN SINGLE CLIENT SLIDE#6 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                      <img src="img/clients_img1.png" alt="clients Brand">
-                    </div>
-                  </div>
-                  <!-- BEGIN SINGLE CLIENT SLIDE#7 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                      <img src="img/clients_img1.png" alt="clients Brand">
-                    </div>
-                  </div>
-                  <!-- BEGIN SINGLE CLIENT SLIDE#8 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                      <img src="img/clients_img1.png" alt="clients Brand">
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <!--=========== END CLIENTS SECTION ================-->
 
 	<!--=========== BEGIN COMMENTS SECTION ================-->
 	<section id="clients">
@@ -294,54 +136,19 @@
               <div class="row">
                 <!-- BEGIN CLIENTS SLIDER -->
                 <div class="clients_slider">
-                  <!-- BEGIN SINGLE CLIENT SLIDE#1 -->
+<!--                   BEGIN SINGLE CLIENT SLIDE#1 
+-->                
+
+                 <?php
+                       $br = count($komentari);
+                       for ($i = 0; $i < $br; $i++){ 
+                                ?> 
                   <div class="col-lg-3 col-md-3 col-sm-3">
                     <div class="single_client">
-                      <p>"Komentar taj i taj" <br>- Korisničko ime korisnika</p>
+                      <p>"<?php echo $komentari[$i]->Tekst ?>" <br>- <?php echo $komentari[$i]->KorisnickoIme ?></p>
                     </div>
                   </div>
-                  <!-- BEGIN SINGLE CLIENT SLIDE#2 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                      <p>"Komentar taj i taj" <br>- Korisničko ime korisnika</p>
-                    </div>
-                  </div>
-                  <!-- BEGIN SINGLE CLIENT SLIDE#3 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                      <p>"Komentar taj i taj" <br>- Korisničko ime korisnika</p>
-                    </div>
-                  </div>
-                  <!-- BEGIN SINGLE CLIENT SLIDE#4 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                      <p>"Komentar taj i taj" <br>- Korisničko ime korisnika</p>
-                    </div>
-                  </div>
-                   <!-- BEGIN SINGLE CLIENT SLIDE#5 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                      <p>"Komentar taj i taj" <br>- Korisničko ime korisnika</p>
-                    </div>
-                  </div>
-                  <!-- BEGIN SINGLE CLIENT SLIDE#6 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                     <p>"Komentar taj i taj" <br>- Korisničko ime korisnika</p>
-                    </div>
-                  </div>
-                  <!-- BEGIN SINGLE CLIENT SLIDE#7 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                      <p>"Komentar taj i taj" <br>- Korisničko ime korisnika</p>
-                    </div>
-                  </div>
-                  <!-- BEGIN SINGLE CLIENT SLIDE#8 -->
-                  <div class="col-lg-3 col-md-3 col-sm-3">
-                    <div class="single_client">
-                     <p>"Komentar taj i taj" <br>- Korisničko ime korisnika</p>
-                    </div>
-                  </div>
+                <?php } ?>
                 </div>
               </div>
             </div>
