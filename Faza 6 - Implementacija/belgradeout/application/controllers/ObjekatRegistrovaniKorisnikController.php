50db31bfdf987269d5fd803e9c9b537b7e0f860b
StrahinjaMilovanovic <ms140463d@student.etf.bg.ac.rs>
2017-06-11 02:29:30 +0200

    Kontrolor za objekat prilikom sesije registrovanog korisnika
    Implementirano je ucitavanje informacija o objektu, prikazivanje aktuelnih dogadjaja u tom objektu i prikazivanje komentara.
    Trebam jos da uradim komentarisanje objekta.

--- Faza 6 - Implementacija/belgradeout/application/controllers/ObjekatRegistrovaniKorisnikController.php
+++ Faza 6 - Implementacija/belgradeout/application/controllers/ObjekatRegistrovaniKorisnikController.php
@@ -1,9 +1,32 @@
 <?php
-
+/**
+ * Description of PrijavaZaModeratoraModel
+ *
+ * @author Strahinja Milovanovic 14/0463
+ */
 class ObjekatRegistrovaniKorisnikController extends CI_Controller{
     public function index(){
+        $idObjekat =  $this->uri->segment(3);
+        
+        $this->load->database();
+        $this->load->model('ObjekatRegistrovaniKorisnikModel');
+        
+        $res = $this->ObjekatRegistrovaniKorisnikModel->DohvatiObjekatIzBaze($idObjekat);
+        
+        $data= $res;
+        
+        $res= $this->ObjekatRegistrovaniKorisnikModel->DohvatiProsecnuOcenu($idObjekat);
+        $data['Ocena']= $res['Ocena'];
+        $data['BrGlasova']= $res['BrGlasova'];
+        
+        $res = $this->ObjekatRegistrovaniKorisnikModel->DohvatiDogadjajeIzBaze($idObjekat);
+        $data['dogadjaji']= $res;
+        
+        $res = $this->ObjekatRegistrovaniKorisnikModel->DohvatiKomentareIzBaze($idObjekat);
+        $data['komentari']= $res;
+        
         $this->load->view('templates/header');
-        $this->load->view('objekat_registrovani_korisnik');
+        $this->load->view('objekat_registrovani_korisnik', $data);
         $this->load->view('templates/footer');
     }
 }
