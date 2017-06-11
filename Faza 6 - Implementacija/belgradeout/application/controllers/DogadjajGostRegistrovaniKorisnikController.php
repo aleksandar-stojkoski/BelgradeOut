cbcbad69f0de8ca9fceb4389171e11bc849c81f6
StrahinjaMilovanovic <ms140463d@student.etf.bg.ac.rs>
2017-06-11 02:21:51 +0200

    Implementiran kontroler za dogadjaje za registrovanog korisnika i gosta

--- Faza 6 - Implementacija/belgradeout/application/controllers/DogadjajGostRegistrovaniKorisnikController.php
+++ Faza 6 - Implementacija/belgradeout/application/controllers/DogadjajGostRegistrovaniKorisnikController.php
@@ -1,9 +1,26 @@
 <?php
-
+/**
+ * Description of PrijavaZaModeratoraModel
+ *
+ * @author Strahinja Milovanovic 14/0463
+ */
 class DogadjajGostRegistrovaniKorisnikController extends CI_Controller{
     public function index(){
+        $idDogadjaj =  $this->uri->segment(3);
+        
+        $this->load->database();
+        $this->load->model('DogadjajGostRegistrovaniKorisnikModel');
+        
+        $res = $this->DogadjajGostRegistrovaniKorisnikModel->DohvatiDogadjajIzBaze($idDogadjaj);
+        
+        $data= $res;
+        $idObjekta= $res['Objekat'];
+        
+        $res= $this->DogadjajGostRegistrovaniKorisnikModel->DohvatiObjekatIzBaze($idObjekta);
+        $data['NazivObjekta']= $res->Naziv;        
+        
         $this->load->view('templates/header');
-        $this->load->view('dogadjaj_gost_registrovani_korisnik');
+        $this->load->view('dogadjaj_gost_registrovani_korisnik', $data);
         $this->load->view('templates/footer');
     }
 }
